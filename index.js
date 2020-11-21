d705f4f3df9ac6cbc00646b5183828be3941b3a5
enevarez-ops <enevarez419@gmail.com>
2020-11-21 17:00:48 -0600

    added questions to index.js file

--- index.js
+++ index.js
@@ -5,10 +5,45 @@ const questions = [
     name: "title",
     message: "What is the title of your project?",
   },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "What are the installation instructions for your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage fo~r your project?",
+  },
+  {
+    type: "input",
+    name: "cont",
+    message: "Who contributed to your project",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What tests are performed for your project?",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "What licenses is your project using?",
+  },
 ];
+
+// console.log(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+
+
 }
 
 // function to initialize program
